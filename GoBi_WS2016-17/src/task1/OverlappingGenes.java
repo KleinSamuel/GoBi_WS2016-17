4747128340ba380249c91116bb988a6fc4c5f27f
Dennis <postd@cip.ifi.lmu.de>
2016-10-25 16:12:49 +0200

    blabla

---
+++ GoBi_WS2016-17/src/task1/OverlappingGenes.java
@@ -0,0 +1,19 @@
+package task1;
+
+import genomeAnnotation.GenomeAnnotation;
+
+public class OverlappingGenes {
+
+	private GenomeAnnotation ga;
+	private String outputDirectory;
+
+	public OverlappingGenes(GenomeAnnotation ga, String outputDir) {
+		this.ga = ga;
+		outputDirectory = outputDir;
+	}
+
+	public void writeOverlappingGenesToFile(){
+		//for(C)
+	}
+	
+}
