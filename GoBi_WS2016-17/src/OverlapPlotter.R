510bc7dd8f906cd1dde7f0f724490ef16bb792a4
Dennis <postd@cip.ifi.lmu.de>
2016-11-20 23:07:42 +0100

    fixed plot for 4

--- GoBi_WS2016-17/src/OverlapPlotter.R
+++ GoBi_WS2016-17/src/OverlapPlotter.R
@@ -12,29 +12,26 @@ if(length(same)>1){
 same = same[-1]
 sameVal = sameVal[-1]
 }
-
 diff = as.numeric(scan(args[1], nlines = 1, blank.lines.skip = TRUE, what = "int", skip = 3))
 diffVal = cumsum(as.numeric(scan(args[1], nlines = 1, blank.lines.skip = TRUE, what = "int", skip = 4)))
 if(length(diff)>1){
 diff = diff[-1]
 diffVal = diffVal[-1]
 }
-
 disregard = as.numeric(scan(args[1], nlines = 1, blank.lines.skip = TRUE, what = "int", skip = 6))
 disregardVal = cumsum(as.numeric(scan(args[1], nlines = 1, blank.lines.skip = TRUE, what = "int", skip = 7)))
 if(length(disregard)>1){
 disregard = disregard[-1]
 disregardVal = disregardVal[-1]
 }
-
 maxX = max(same, diff, disregard)
 maxY = max(sameVal, diffVal, disregardVal)
 
 png(args[2])
-
-plot(same, sameVal, type = "l", xlim = c(0, maxX), ylim = c(0, maxY), col = "red", main=biotype, xlab = "#overlapping genes", ylab = "#genes with x overlapping genes(cumulative)")
-lines(diff, diffVal, col = "blue")
-lines(disregard, disregardVal, col = "black")
-legend("toprigth", c("sameStrand", "differentStrand", "disregardingStrand"), lty=c(1,1,1), col = c("red","blue","black"), inset=c(0,0))
+par(mar=c(5, 5, 5, 10), xpd=TRUE)
+plot(same, sameVal, type = "o", xlim = c(0, maxX), ylim = c(0, maxY+1), pch = 0, col = "red", main=biotype, xlab = "#overlapping genes", ylab = "#genes with x overlapping genes(cumulative)")
+lines(diff, diffVal, col = "blue", pch = 1, type = "o")
+lines(disregard, disregardVal, col = "black", pch = 2, type = "o")
+legend("topright", c("sameStrand", "differentStrand", "disregardingStrand"), lty=c(1,1,1), pch = c(0,1,2), col = c("red","blue","black"), inset = c(-0.55, 0))
 
 dev.off()
