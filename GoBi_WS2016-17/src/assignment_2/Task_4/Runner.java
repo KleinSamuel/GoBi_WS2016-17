5f86f04df421b5caae0b3e2bce0049fff176c3e8
Dennis <postd@cip.ifi.lmu.de>
2016-12-06 18:15:43 +0100

    nothing changed

--- GoBi_WS2016-17/src/assignment_2/Task_4/Runner.java
+++ GoBi_WS2016-17/src/assignment_2/Task_4/Runner.java
@@ -3,6 +3,7 @@ package assignment_2.Task_4;
 public class Runner {
 
 	public static void main(String[] args) {
+		// bamsListFile mainDirectory mappingInfo GTF
 		MultiBAMFileReader multi = new MultiBAMFileReader(args[0], args[1], args[2], args[3]);
 	}
 
